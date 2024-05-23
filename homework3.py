837618206faad4d577168f5c0b64a1464ceeb259
kira9137 <kira9137@gmail.com>
2024-05-23 09:43:49 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -3,6 +3,6 @@ print("Имя: ", name)
 age = 27
 print("Возраст: ", age)
 age += 1
-print("Возраст + 1: ", age)
+print("В следующем году: ", age)
 is_student = True
 print("Ты студент?: ", is_student)
