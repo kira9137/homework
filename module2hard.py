3ace0ba6263e044747022ed181fd8c4f0d865c3d
kira9137 <kira9137@gmail.com>
2024-06-01 13:58:13 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,10 @@
+def find_pairs(target):
+    pairs = []  
+    for i in range(1, 20):
+        for j in range(i + 1, 20):
+            if i != target and j != target and  i != j and target % (i + j) == 0:
+                pairs.append((i, j))
+    return pairs
+
+for target in range(3, 21):
+    print(f'Число {target} - {find_pairs(target)}')
