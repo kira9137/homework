65bffb710d73d42066e3dd13bda20585713463fd
kira9137 <kira9137@gmail.com>
2024-06-09 08:15:43 +0300

    Update module_2_6.py

--- module_2_6.py
+++ module_2_6.py
@@ -1,7 +1,7 @@
 def single_root_words(root_word, *other_words):
   same_words = []
   for i in other_words:
-      if root_word.lower() in i.lower() or root_word.upper() in i.upper():
+      if root_word.lower() in i.lower() or i.lower() in root_word.lower():
           same_words.append(i)
   return same_words
 
