bad915bf8ae9978d3e92bbdeadebcc7be949d67c
kira9137 <kira9137@gmail.com>
2024-06-27 09:22:14 +0300

    Delete module_4_1.py

--- module_4_1.py
+++
@@ -1,9 +0,0 @@
-from fake_math import divide as fake_divide
-from true_math import divide as true_divide
-
-# Пример использования
-fake_result = fake_divide(10, 0)
-true_result = true_divide(10, 0)
-
-print(f"Результат fake_math: {fake_result}")
-print(f"Результат true_math: {true_result}")
